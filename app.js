995da60d28ca0e111b12213635940ec637e00d00
hansolpark <dundunki@naver.com>
2023-02-08 15:52:37 +0900

    fix: router 구현

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const ajax = new XMLHttpRequest();
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
+const container = document.getElementById("root");
+
 function getData(url) {
   ajax.open("GET", url, false);
   ajax.send();
@@ -10,14 +12,23 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-console.log("newsFeed", newsFeed);
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
+  newsList.push("<ul>");
 
-const container = document.getElementById("root");
-const ul = document.createElement("ul");
-const content = document.createElement("div");
+  for (let i = 0; i < 2; i++) {
+    newsList.push(`
+          <li>
+          <a href="#${newsFeed[i].id}"> ${newsFeed[i].title} 댓글(${newsFeed[i].comments_count})</a>
+          </li>`);
+  }
+  newsList.push("</ul>");
 
-window.addEventListener("hashchange", () => {
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
   console.log("hash 변경됨");
   // id 가져오기
   const id = location.hash.substring(1);
@@ -25,22 +36,22 @@ window.addEventListener("hashchange", () => {
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
   container.innerHTML = `
-        <h1>${newsContent.title}</h1>
-        <div>
-            <a href="#">목록으로</a>
-        </div>
-    `;
-});
-
-const newsList = [];
-newsList.push("<ul>");
-
-for (let i = 0; i < 10; i++) {
-  newsList.push(`
-    <li>
-    <a href="#${newsFeed[i].id}"> ${newsFeed[i].title} 댓글(${newsFeed[i].comments_count})</a>
-    </li>`);
+          <h1>${newsContent.title}</h1>
+          <div>
+              <a href="/">목록으로</a>
+          </div>
+      `;
 }
-newsList.push("</ul>");
 
-container.innerHTML = newsList.join("");
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+window.addEventListener("hashchange", newsDetail);
+
+router();
