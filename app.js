90bfadd2596019594d09b8288be565d4adb451c7
hansolpark <dundunki@naver.com>
2023-02-08 12:44:49 +0900

    fix: 해시변경시 addEventListener

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const ajax = new XMLHttpRequest();
 
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+
 ajax.open("GET", NEWS_URL, false);
 ajax.send();
 
@@ -9,9 +11,27 @@ console.log("newsFeed", newsFeed);
 
 const ul = document.createElement("ul");
 
+window.addEventListener("hashchange", () => {
+  console.log("hash 변경됨");
+  // id 가져오기
+  const id = location.hash.substring(1);
+
+  // content 가져오기
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+  const newsContent = JSON.parse(ajax.response);
+
+  console.log("newsContent", newsContent);
+});
+
 for (let i = 0; i < 10; i++) {
   const li = document.createElement("li");
-  li.innerHTML = newsFeed[i].title;
+  const a = document.createElement("a");
+
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} 댓글(${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
