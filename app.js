f54e182e549ccf19440dee11a8ce541c36b13dc7
hansolpark <dundunki@naver.com>
2023-02-08 12:06:29 +0900

    fix: for문으로 element 추가

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const ajax = new XMLHttpRequest();
 
-ajax.open("GET", "https://api.hnpwa.com/v0/news/1.json", false);
+const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+ajax.open("GET", NEWS_URL, false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
 console.log("newsFeed", newsFeed);
 
-document.getElementById("root").innerHTML = `<ul>   
-    <li>${newsFeed[0].title}</li>
-    <li>${newsFeed[1].title}</li>
-    <li>${newsFeed[2].title}</li>
-</ul>`;
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 10; i++) {
+  const li = document.createElement("li");
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById("root").appendChild(ul);
