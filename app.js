ad61bddd766b44d7785bbb23eda97b75d33667fd
hansolpark <dundunki@naver.com>
2023-02-09 19:09:23 +0900

    fix: 상세페이지 ui css 적용

--- app.js
+++ app.js
@@ -86,12 +86,33 @@ function newsDetail() {
   // content 가져오기
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
-  container.innerHTML = `
-          <h1>${newsContent.title}</h1>
-          <div>
-              <a href="#/page/${store.currentPage}">목록으로</a>
-          </div>
-      `;
+  let template = `
+    <div class="bg-gray-600 min-h-screen pb-8">
+        <div class="bg-white text-xl">
+            <div class="mx-auto px-4">
+                <div class="flex justify-between items-center py-6">
+                    <div class="flex justify-start">
+                        <h1 class="font-extrabold">Hacker News</h1>
+                    </div>
+                    <div class="items-center justify-end">
+                        <a href="#/page/${store.currentPage}" class="text-gray-500">
+                            목록으로
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="h-full border rounded-xl bg-white m-6 p-4">
+                <h2>${newsContent.title}</h2>
+                <div class="text-gray-400 h-20">
+                     ${newsContent.content}
+                </div>
+            </div>
+            {{__comments__}}
+        </div>
+    </div>
+`;
+
+  container.innerHTML = template;
 }
 
 function router() {
