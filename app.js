784f1167173952f3c1c8e99098228ab2c7d65672
hansolpark <dundunki@naver.com>
2023-02-09 15:34:31 +0900

    fix: 페이징 처리

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
 const container = document.getElementById("root");
 
+const store = {
+  currentPage: 1,
+};
+
 function getData(url) {
   ajax.open("GET", url, false);
   ajax.send();
@@ -17,13 +21,21 @@ function newsFeed() {
   const newsList = [];
   newsList.push("<ul>");
 
-  for (let i = 0; i < 2; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
           <li>
-          <a href="#${newsFeed[i].id}"> ${newsFeed[i].title} 댓글(${newsFeed[i].comments_count})</a>
+          <a href="#/show/${newsFeed[i].id}"> ${newsFeed[i].title} 댓글(${newsFeed[i].comments_count})</a>
           </li>`);
   }
   newsList.push("</ul>");
+  newsList.push(`
+    <div>
+        <a href="#/page/${
+          store.currentPage > 1 ? store.currentPage - 1 : 1
+        }">이전페이지</a>
+        <a href="#/page/${store.currentPage + 1}">다음페이지</a>
+    </div>
+  `);
 
   container.innerHTML = newsList.join("");
 }
@@ -31,14 +43,14 @@ function newsFeed() {
 function newsDetail() {
   console.log("hash 변경됨");
   // id 가져오기
-  const id = location.hash.substring(1);
+  const id = location.hash.substring(7);
   // content 가져오기
   const newsContent = getData(CONTENT_URL.replace("@id", id));
 
   container.innerHTML = `
           <h1>${newsContent.title}</h1>
           <div>
-              <a href="/">목록으로</a>
+              <a href="#/page/${store.currentPage}">목록으로</a>
           </div>
       `;
 }
@@ -48,10 +60,13 @@ function router() {
 
   if (routePath === "") {
     newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substring(7));
+    newsFeed();
   } else {
     newsDetail();
   }
 }
-window.addEventListener("hashchange", newsDetail);
+window.addEventListener("hashchange", router);
 
 router();
